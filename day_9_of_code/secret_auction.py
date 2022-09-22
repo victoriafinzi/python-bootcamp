88b7d6895e588539dffab70b68b8517d23c776c9
victoriafinzi <victoria.finzi15@gmail.com>
2022-09-22 18:56:53 -0300

    secret-auction code done

--- day_9_of_code/secret_auction.py
+++ day_9_of_code/secret_auction.py
@@ -4,24 +4,27 @@ print(logo)
 print("Welcome to the secret auction program.")
 
 should_continue = True
-while should_continue:
-    auction = []
-    names = input("What is your name?: ")
-    bids = int(input("What is your bid?: $"))
-    
-    def add_persons(names, bids):
-        add_person = {}
-        add_person["name"] = names
-        add_person["bid"] = bids
-        auction.append(add_person)
+bids = {}
 
+def find_highest_bidder(bidding_record):
+    highest_bid = 0
+    winner = ""
+    for bidder in bidding_record:
+        bid_amount = bidding_record[bidder]
+        if bid_amount > highest_bid:
+            highest_bid = bid_amount
+            winner = bidder
+    print(f"The winner is {winner} with the bid of ${highest_bid}")
+
+while should_continue:
+    name = input("What is your name?: ")
+    price = int(input("What is your bid?: $"))
+    bids[name] = price
     
-    add_persons(names, bids)
-    
-    add_more_bids = input("Are there any other bidders? Type 'yes' or 'no'.")
-    clear()
-    print(auction)
+    add_more_bids = input("Are there any other bidders? Type 'yes' or 'no'.\n")
+
     if add_more_bids == "no":
         should_continue = False
-        max_value = max(auction)
-        print(max_value)
+        find_highest_bidder(bids)
+    elif add_more_bids == "yes":
+        clear()
--- day_9_of_code/travel_log.py
+++ day_9_of_code/travel_log.py
@@ -27,21 +27,21 @@ add_new_states("Ceara", ["Jericoacoara"])
 print(travel_log)
 
 #Como adicionar uma nova chave&valor no dicionário
-# starting_dictionary = {
-#     "a": 9,
-#     "b": 8,
-# }
+starting_dictionary = {
+    "a": 9,
+    "b": 8,
+}
 
 
-# final_dictionary = {
-#     "a": 9,
-#     "b": 8,
-#     "c": 7,
-# }
+final_dictionary = {
+    "a": 9,
+    "b": 8,
+    "c": 7,
+}
 
-# starting_dictionary["c"] = 7
-# final_dictionary = starting_dictionary
-# print(final_dictionary)
+starting_dictionary["c"] = 7
+final_dictionary = starting_dictionary
+print(final_dictionary)
 
 order = {
     "starter": {1: "Salad", 2: "Soup"},
